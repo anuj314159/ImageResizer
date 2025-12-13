684b148a941d5dcc114ff66e73b162d0fbf35daf
anujk3.1415 <anujk3.1415@gmail.com>
2025-12-13 21:11:05 +0530

    just random stuff for initialized app commit 0

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace ImageEdit;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        using (OpenFileDialog dialog = new OpenFileDialog())
+        {
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif;*.tiff;*.ico";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(dialog.FileName);
+            }
+        }
+    }
+}
